5f329f702bc0ae923c8b644088c44cb196956995
Albert Torres <albrewot@gmail.com>
2019-07-18 01:59:49 -0400

    [Inicial] Commit inicial con el servidor que ya se tenia desde clases anteriores

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// console.log(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/test`);
+// mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/test`, {
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${
+    process.env.DB_PASSWORD
+  }@cursonode-yulba.mongodb.net/test?retryWrites=true&w=majority`,
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
+
+mongoose.Promise = global.Promise;
+
+module.exports = {
+  User: require("../models/User")
+};
